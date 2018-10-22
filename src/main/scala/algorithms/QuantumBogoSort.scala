91a042d6112573b11d641b154904fb654f67035d
PhaseRush <leozhang888@yahoo.ca>
2018-10-22 11:17:37 -0700

    Finally added that baka into universe......

--- src/main/scala/algorithms/QuantumBogoSort.scala
+++ src/main/scala/algorithms/QuantumBogoSort.scala
@@ -10,15 +10,16 @@ object QuantumBogoSort {
 
   def sortArray(array: Array[Int]): Array[Int] = {
     while (true) {
-      val currentUniverse = new Universe(array, dannieboop = currentDannie)
+      val currentUniverse = new Universe(array, dannieboop = new Danthonywalker5512)
       val currentDannie = currentUniverse.dannie
       Util.quantumRandomize(currentUniverse.sortedArray)
 
       if (!Util.arrayIsSorted(currentUniverse.sortedArray)) {
-        currentUniverse.delete()
         currentUniverse.dannie.idontlikeyou()
+        currentUniverse.delete()
       }
       else return currentUniverse.sortedArray
     }
+    return null
   }
 }
